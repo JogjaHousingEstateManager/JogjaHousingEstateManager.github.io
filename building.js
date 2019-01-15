2c22fc3602bd701ae44c540191c50c7589954bbf
JogjaHousingEstateManager <46705113+JogjaHousingEstateManager@users.noreply.github.com>
2019-01-15 21:06:12 +1300

    Create building.js

---
+++ building.js
@@ -0,0 +1,54 @@
+/*
+ *
+ * Copyright 2019 Dtji Psimfans (Admin and creator of "Global App Developers @ Jogja" Facebook group)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+*/
+
+function Building(name, description, amount, coinCost, IDRCost, mCubeCost, kWhCost, workerCost, visitorCost, emptySpaceCost, specialCoinCost, coinProductionRate, IDRProductionRate, mCubeProductionRate, kWhProductionRate, workerProductionRate, visitorProductionRate, emptySpaceProductionRate, specialCoinProductionRate, actualCoinProductionRate, actualIDRProductionRate actualmCubeProductionRate, actualkWhProductionRate, actualWorkerProductionRate, actualVisitorProductionRate, actualEmptySpaceProductionRate, actualSpecialCoinProductionRate, requirements, unlocked){
+    this.name = name;
+    this.description = description;
+    this.amount = amount;
+    this.coinCost = coinCost;
+    this.IDRCost = IDRCost;
+    this.mCubeCost = mCubeCost;
+    this.kWhCost = kWhCost;
+    this.workerCost = workerCost;
+    this.visitorCost = visitorCost;
+    this.emptySpaceCost = emptySpaceCost;
+    this.specialCoinCost = specialCoinCost;
+    this.coinProductionRate = coinProductionRate;
+    this.IDRProductionRate = IDRProductionRate;
+    this.mCubeProductionRate = mCubeProductionRate;
+    this.kWhProductionRate = kWhProductionRate;
+    this.workerProductionRate = workerProductionRate;
+    this.visitorProductionRate = visitorProductionRate;
+    this.emptySpaceProductionRate = emptySpaceProductionRate;
+    this.specialCoinProductionRate = specialCoinProductionRate;
+    this.actualCoinProductionRate = actualCoinProductionRate;
+    this.actualIDRProductionRate = actualIDRProductionRate;
+    this.actualmCubeProductionRate = actualmCubeProductionRate;
+    this.actualkWhProductionRate = actualkWhProductionRate;
+    this.actualWorkerProductionRate = actualWorkerProductionRate;
+    this.actualVisitorProductionRate = actualVisitorProductionRate;
+    this.actualEmptySpaceProductionRate = actualEmptySpaceProductionRate;
+    this.actualSpecialCoinProductionRate = actualSpecialCoinProductionRate;
+    this.requirements = requirements;
+    this.unlocked = unlocked;
+};
