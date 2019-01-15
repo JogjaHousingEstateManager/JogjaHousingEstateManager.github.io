62bd2f6200b5d971c2e74f992328c1357b0dfe40
JogjaHousingEstateManager <46705113+JogjaHousingEstateManager@users.noreply.github.com>
2019-01-15 21:29:29 +1300

    Create player.js

---
+++ player.js
@@ -0,0 +1,12 @@
+var player =  {
+    // Your resources
+    coins: new Decimal(0),
+    IDR: new Decimal(0),
+    mCube: new Decimal(0),
+    kWh: new Decimal(0),
+    workers: new Decimal(0),
+    visitors: new Decimal(0),
+    emptySpace: new Decimal(0),
+    specialCoin: new Decimal(0),
+    // Places to be owned
+};
